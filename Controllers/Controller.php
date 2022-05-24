eb634fca32ca2e0ec6f395559a2fb10e769f45e6
Saulo Candeira <61151496+SauloCandeira@users.noreply.github.com>
2022-05-24 11:22:19 -0300

    Add files via upload

---
+++ Controllers/Controller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function homepage()
+    {
+        return view('welcome');
+    }
+    public function cadastrar()
+    {
+        echo 'Tela de cadastro';
+    }
+    public function fazerloginr()
+    {
+        echo 'Tela de login';
+    }
+
+}
